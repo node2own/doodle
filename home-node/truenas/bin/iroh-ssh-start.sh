740b7c57ed56ee37f2c0de10e8b120dfbdae8ba8
Jeroen van Maanen <jeroen@leialearns.org>
2025-09-27 15:46:29 +0200

    Added `iroh-ssh` support

--- home-node/truenas/bin/iroh-ssh-start.sh
+++ home-node/truenas/bin/iroh-ssh-start.sh
@@ -29,7 +29,7 @@ docker rm -f "${IMAGE}" >/dev/null 2>&1 || true
 DOCKER_RUN_FLAGS=()
 DOCKER_RUN_FLAGS+=(-v "$(dirname "${BIN}")/etc/iroh-ssh:/home")
 
-NOBODY_UID="$(uid -u nobody)"
+NOBODY_UID="$(id -u nobody)"
 NOGROUP_GUID="$(getent group nogroup | cut -d: -f3)"
 read -r -d '' IROH_SSH_INIT <<EOT || true
 mkdir -p /home/iroh-ssh-local
@@ -37,4 +37,4 @@ chown '${NOBODY_UID}:${NOGROUP_GUID}' /home/iroh-ssh-local
 EOT
 docker run --rm "${DOCKER_RUN_FLAGS[@]}" "${IMAGE}:${TAG}" "${IROH_SSH_INIT}"
 
-docker run --rm --name "${IMAGE}" --network host "${DOCKER_RUN_FLAGS[@]}" "${IMAGE}:${TAG}" /iroh-ssh server --persist
+docker run --rm --name "${IMAGE}" --network host -u "${NOBODY_UID}" "${DOCKER_RUN_FLAGS[@]}" "${IMAGE}:${TAG}" /iroh-ssh server --persist
