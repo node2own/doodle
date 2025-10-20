c15be32c7abb96b7e6d4b5ba82e9d4929e771b69
Jeroen van Maanen <jeroen@sollunae.net>
2025-10-20 17:39:51 +0000

    Improved docker-image for `dev-rust-dioxus`

--- home-node/truenas/docker/dev-rust-dioxus/dumbpipe.sh
+++ home-node/truenas/docker/dev-rust-dioxus/dumbpipe.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/dumbpipe listen-tcp --host localhost:8080 >/var/log/dumbpipe 2>&1 &
+RUST_LOG='info' /usr/local/bin/dumbpipe listen-tcp --host localhost:8080 --persist >/var/log/dumbpipe.log 2>&1 &
+echo "Consult /var/log/dumbpipe.log for the node-id"
